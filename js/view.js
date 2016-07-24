d3f9033baabd05b968db8265a0ac1325234dca5f
Moritz Fliedner <moritz@stanfordalumni.org>
2016-07-24 14:46:20 -0700

    more panel styling

--- js/view.js
+++ js/view.js
@@ -8,11 +8,26 @@ const View = function (canvas) {
 
 View.prototype.init = function() {
   $( ".panel" ).draggable();
+  $( "#introduction_body" ).hide();
+  document.getElementById('hide_introduction').innerHTML = "show";
   $('#hide_introduction').click(function() {
-    $("#introduction_panel").slideUp();
+    if ( $( "#introduction_body" ).is( ":hidden" ) ) {
+      $( "#introduction_body" ).show( "slow" );
+      document.getElementById('hide_introduction').innerHTML = "hide";
+    } else {
+      $( "#introduction_body" ).slideUp();
+      document.getElementById('hide_introduction').innerHTML = "show";
+    }
   });
+  document.getElementById('hide_instructions').innerHTML = "hide";
   $('#hide_instructions').click(function() {
-    $('#instructions_panel').slideUp();
+    if ( $( "#instructions_body" ).is( ":hidden" ) ) {
+      $( "#instructions_body" ).show( "slow" );
+      document.getElementById('hide_instructions').innerHTML = "hide";
+    } else {
+      $( "#instructions_body" ).slideUp();
+      document.getElementById('hide_instructions').innerHTML = "show";
+    }
   });
 };
 
